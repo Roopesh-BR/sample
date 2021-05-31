61e5006dc604dbda98a280a1fe57a67da7241bb2
Roopesh-BR <73352937+Roopesh-BR@users.noreply.github.com>
2021-05-31 20:50:59 +0530

    Add files via upload

--- route_planner.cpp
+++ route_planner.cpp
@@ -1,15 +1,15 @@
 #include"route_planner.h"
 #include<algorithm>
 
-RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y){
+RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y): m_Model(model){
 
     start_x *= 0.01;
     start_y *= 0.01;
-    end_x *= 0.01;
+    end_x *= 0.01;  
     end_y *= 0.01;
 
-    start_node = &model.FindClosestNode(start_x, start_y);
-    end_node = &model.FindClosestNode(end_x, end_y);
+    start_node = &m_Model.FindClosestNode(start_x, start_y);
+    end_node = &m_Model.FindClosestNode(end_x, end_y);
 
 }
 
